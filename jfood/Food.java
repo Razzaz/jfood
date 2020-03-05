b45da39562331cfe459a4a46ab88a6eee019282f
Ridho Gani <ridhoadhadigani@gmail.com>
2020-03-05 16:40:21 +0700

    modul 1

---
+++ jfood/Food.java
@@ -0,0 +1,68 @@
+
+/**
+ * Write a description of class Food here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Food
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private String name;
+    private int price;
+    private String category;
+    
+
+    /**
+     * Constructor for objects of class Food
+     */
+    public Food()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getId()
+    {
+        // put your code here
+        return 0;
+    }
+    public String getName()
+    {
+        // put your code here
+        return "oke";
+    }
+    public int getPrice()
+    {
+        // put your code here
+        return 0;
+    }
+    public String getCategory()
+    {
+        // put your code here
+        return "oke";
+    }
+    public void setId(int id)
+    {
+       
+    }
+    public void setName(String name)
+    {
+       
+    }
+    public void setPrice(int price)
+    {
+       
+    }
+    public void setCategory(String category)
+    {
+       
+    }
+}
---
+++ jfood/JFood.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class JFood here.
+ *
+ * @author Ridho Gani
+ * @version 5/3/2020
+ */
+public class JFood
+{
+    
+    public static void main(String args[])
+    {
+        // initialise instance variables
+       
+    }
+
+}
